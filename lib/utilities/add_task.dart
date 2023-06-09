6c3d1ae7c62f0dc9de59d597ccb83313135124a1
Swagnik02 <swagnik1234@gmail.com>
2023-06-09 20:11:23 +0530

    added text fields and buttons in addTask

--- lib/utilities/add_task.dart
+++ lib/utilities/add_task.dart
@@ -13,9 +13,51 @@ class _AddTaskState extends State<AddTask> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('New Task'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('New Task'),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Container(
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Title',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Description',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(backgroundColor:
+                        MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Color.fromARGB(255, 32, 132, 37);
+                      }
+                      return Theme.of(context).primaryColor;
+                    })),
+                    child: const Text(
+                      'Add Task',
+                      // style: GoogleFonts.roboto(fontSize: 18),
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            )));
   }
 }
