6e59af15c58acdb5e1c0f056171e1aea59eac7d3
Swagnik02 <swagnik1234@gmail.com>
2023-06-09 14:28:15 +0530

    Initial Home, firebase added

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/home.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.purple,
+      ),
+    );
+  }
+}
